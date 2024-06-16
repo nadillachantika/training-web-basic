7591cf55cb4dc9a7227bc2dd9a5037d4e24ada7f
Nadilla C. Putri <58943422+nadillachantika@users.noreply.github.com>
2024-06-16 14:57:39 +0700

    first commit

---
+++ WebBasic-Miniproject/script.js
@@ -0,0 +1,4 @@
+// script.js
+function displayMessage() {
+    document.getElementById('contact-message').innerText = 'Terima kasih telah menghubungi saya! Saya akan segera menghubungi Anda kembali.';
+}
